63d169ca0483c49a1ecef0efc5f5eca6d2323ce2
Vinicius Pegorini <viniciuspegorini@gmail.com>
2023-03-13 22:52:27 -0300

    aula1 - 2023-03-13 (UsertrollerTest saving user, returning message )

--- aula1/server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
+++ aula1/server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -2,6 +2,10 @@ package br.edu.utfpr.pb.pw25s.server.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,8 +13,14 @@ import lombok.*;
 @ToString
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 50)
     private String username;
 
+    @Column(length = 50, name = "display_name")
     private String displayName;
 
     private String password;
